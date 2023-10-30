773965afe58fbc764f1914fb3c0b4519bee619cc
moshebeutel <moshebeutel@gmail.com>
2023-10-30 08:32:36 +0200

    Add validation split

--- dataloaders/cifar10_loader.py
+++ dataloaders/cifar10_loader.py
@@ -1,22 +1,25 @@
 import os.path
 import torchvision.transforms as transforms
 from torchvision.datasets import CIFAR10
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 
 
-def load_data(root: str, batch_size=32, ood=False):
+def load_data(root: str, batch_size: int = 32,
+              validation_split: float = 0.1, ood: bool = False) -> tuple[DataLoader, DataLoader, DataLoader]:
     """
     Load CIFAR-10 dataset.
 
     Parameters:
     root (str): Root directory where the dataset will be stored.
     batch_size (int, optional): Batch size for data loaders. Defaults to 32.
+    validation_split (float, optional): validation subset fraction in (0,1) interval out of train set. Defaults to 0.1.
     ood (bool, optional): Flag indicating if the data is out-of-distribution (OOD). Defaults to False.
 
     Returns:
     Tuple[DataLoader, DataLoader]: DataLoader for the training set and DataLoader for the test set.
     """
     assert os.path.exists(root), f'{root} does not exist'
+    assert 0.0 < validation_split < 1.0, f'Expected validation_split in (0,1) interval. Got {validation_split}'
 
     transform = transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
@@ -27,7 +30,10 @@ def load_data(root: str, batch_size=32, ood=False):
 
     print(f'load_data to {root}, ood? {ood}')
     train_set = CIFAR10(root, train=True, download=True, transform=transform)
+    validation_set_size = int(len(train_set) * validation_split)
+    val_set = Subset(train_set, range(0, validation_set_size))
     test_set = CIFAR10(root, train=False, download=True, transform=transform)
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_set, batch_size=batch_size)
     test_loader = DataLoader(test_set, batch_size=32)
-    return train_loader, test_loader
+    return train_loader, val_loader, test_loader
--- dataloaders/data_loaders_factory.py
+++ dataloaders/data_loaders_factory.py
@@ -1,9 +1,12 @@
 import os
 
+from torch.utils.data import DataLoader
+
 from dataloaders.cifar10_loader import load_data
 
 
-def get_data_loaders(data_path: str, batch_size: int):
+def get_data_loaders(data_path: str,
+                     batch_size: int) -> tuple[DataLoader, DataLoader, DataLoader, DataLoader, DataLoader, DataLoader]:
     """
     Get data loaders for CIFAR-10 dataset.
 
@@ -12,16 +15,17 @@ def get_data_loaders(data_path: str, batch_size: int):
     batch_size (int): Batch size for the data loaders.
 
     Returns:
-    Tuple[DataLoader, DataLoader, DataLoader, DataLoader]: Data loaders for training set,
-                                                           out-of-distribution training set,
+    Tuple[DataLoader, DataLoader, DataLoader, DataLoader, DataLoader, DataLoader]: Data loaders for training set,
+                                                           out-of-distribution training set, validation set,
+                                                           out-of-distribution validation set,
                                                            test set, and out-of-distribution test set.
     """
     assert os.path.exists(data_path), f'Path given to data does not exist. Got {data_path}'
 
     # Load data for standard training and testing
-    train_loader, test_loader = load_data(root=data_path, batch_size=batch_size, ood=False)
+    train_loader, val_loader, test_loader = load_data(root=data_path, batch_size=batch_size, ood=False)
 
     # Load data for out-of-distribution (OOD) training and testing
-    train_loader_ood, test_loader_ood = load_data(root=data_path, batch_size=batch_size, ood=True)
+    train_loader_ood, val_loader_ood, test_loader_ood = load_data(root=data_path, batch_size=batch_size, ood=True)
 
-    return train_loader, train_loader_ood, test_loader, test_loader_ood
+    return train_loader, train_loader_ood, val_loader, val_loader_ood, test_loader, test_loader_ood
